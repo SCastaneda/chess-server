65efc6f27cb33ee955630d4caa4a9df3b9c4b69c
Samuel Castaneda <sammy.castaneda90@gmail.com>
2015-03-18 22:26:08 -0400

    board setup

--- src/chess_server/core.clj
+++ src/chess_server/core.clj
@@ -1,7 +1,44 @@
 (ns chess-server.core
   (:gen-class))
 
+;; define white pieces
+(def wking "wK")
+(def wqueen "wq")
+(def wknight "wk")
+(def wbishop "wb")
+(def wrook "wr")
+(def wpawn "wp")
+
+;; define black pieces
+(def bking "bK")
+(def bqueen "bq")
+(def bknight "bk")
+(def bbishop "bb")
+(def brook "br")
+(def bpawn "bp")
+
+(def empty-s "x") ;; empty square
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn fill-row [n piece] 
+  (cond
+    (= n 0) nil
+    true (cons piece (empty-row (- n 1)))
+   )
+  )
+
+(defn create-new-board []
+  '((wrook wknight wbishop wqueen wking wbishop wknight wrook)
+    (fill-row 8 wpawn)
+    (fill-row 8 empty-s) 
+    (fill-row 8 empty-s) 
+    (fill-row 8 empty-s) 
+    (fill-row 8 empty-s)
+    (fill-row 8 bpawn)
+    (wrook wknight wbishop wqueen wking wbishop wknight wrook)
+    )
+  )
