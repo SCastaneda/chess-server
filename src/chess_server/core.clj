74cc16bf23991ba06d2edf0dfe57a02b9aeffc7d
Samuel Castaneda <sammy.castaneda90@gmail.com>
2015-05-13 00:13:20 -0400

    converted to vectors, added move fn w/out verification

--- src/chess_server/core.clj
+++ src/chess_server/core.clj
@@ -26,25 +26,41 @@
 
 (defn fill-row [n piece] 
   (cond
-    (= n 0) nil
-    true (cons piece (fill-row (- n 1) piece))
+    (= n 0) []
+    true (conj (fill-row (- n 1) piece) piece)
    )
   )
 
 (defn create-new-board []
-  (list 
-   (list wrook wknight wbishop wqueen wking wbishop wknight wrook)
+  [ 
+   [wrook wknight wbishop wqueen wking wbishop wknight wrook]
    (fill-row 8 wpawn)
    (fill-row 8 empty-s) 
    (fill-row 8 empty-s) 
    (fill-row 8 empty-s) 
    (fill-row 8 empty-s)
    (fill-row 8 bpawn)
-   (list brook bknight bbishop bqueen bking bbishop bknight brook)
-   )
+   [brook bknight bbishop bqueen bking bbishop bknight brook]
+   ]
   )
   
 
 (defn get-square [board x y]
   (nth (nth board y) x)
+  )
+
+;; just moves pieces from one position to another, 
+;; overriding the target position (no verification of moves yet)
+(defn move [board fromx fromy tox toy] 
+  (let 
+    [current (get-square board fromx fromy) 
+    future (get-square board tox toy)]
+    (assoc 
+      ;; move piece to new position
+      (assoc board toy (assoc (nth board toy) tox current))
+      fromy
+      ;; put an empty square at the current(from) position
+      (assoc (nth board fromy) fromx empty-s)
+      )
+    )
   )
